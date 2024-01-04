2b06d1d63e159c145b71929286faacc33c01ca3e
Wildan Ghiffarie Budhi <wildangb2@gmail.com>
2024-01-05 00:44:29 +0700

    Change Scheduler to every hours

--- apps/scheduler/main.py
+++ apps/scheduler/main.py
@@ -33,23 +33,11 @@ def submit_task( name: str ):
     logging.info( f"[Scheduler] Submit Task {name}" )
     celery_app.send_task( name=name )
 
-schedule.every().day.at("00:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
-schedule.every().day.at("03:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
-schedule.every().day.at("06:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
-schedule.every().day.at("09:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
-schedule.every().day.at("12:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
-schedule.every().day.at("15:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
-schedule.every().day.at("18:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
-schedule.every().day.at("21:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
+# schedule.every().day.at("05:00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
+# schedule.every().day.at("05:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
 
-schedule.every().day.at("00:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
-schedule.every().day.at("03:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
-schedule.every().day.at("06:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
-schedule.every().day.at("09:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
-schedule.every().day.at("12:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
-schedule.every().day.at("15:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
-schedule.every().day.at("18:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
-schedule.every().day.at("21:00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
+schedule.every().hour.at(":00", "Asia/Jakarta").do( submit_task, name='cot_data_pipeline' )
+schedule.every().hour.at(":00", "Asia/Jakarta").do( submit_task, name='economics_data_pipeline' )
 
 while True:
     schedule.run_pending()
