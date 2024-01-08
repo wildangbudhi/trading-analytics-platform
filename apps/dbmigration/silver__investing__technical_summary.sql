0a93a4ef031c2656f184d0af814d56e5d6ff436f
Wildan Ghiffarie Budhi <wildangb2@gmail.com>
2024-01-09 00:06:13 +0700

    [Task] Add Scraping Investing.com Technical Analysis Summary

--- apps/dbmigration/silver__investing__technical_summary.sql
+++ apps/dbmigration/silver__investing__technical_summary.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS public.silver__investing__technical_summary (
     ts bigint
 );
 
-CREATE INDEX IF NOT EXISTS silver__it__1__idx ON public.silver__investing__technical_summary(date);
+CREATE INDEX IF NOT EXISTS silver__it__1__idx ON public.silver__investing__technical_summary(surr_key);
 CREATE INDEX IF NOT EXISTS silver__it__2__idx ON public.silver__investing__technical_summary(ts);
